9c4004f42fb317734b77d217bd59e5037001ce75
AmanShukla00 <aman17101998@gmail.com>
2025-02-18 00:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".image");
+    let draggedItem = null;
+
+    images.forEach(image => {
+        image.addEventListener("dragstart", function (e) {
+            draggedItem = this;
+            e.dataTransfer.setData("text/plain", "");
+        });
+
+        image.addEventListener("dragover", function (e) {
+            e.preventDefault();
+        });
+
+        image.addEventListener("drop", function (e) {
+            e.preventDefault();
+            if (draggedItem !== this) {
+                let temp = this.innerHTML;
+                this.innerHTML = draggedItem.innerHTML;
+                draggedItem.innerHTML = temp;
+            }
+        });
+    });
+});
